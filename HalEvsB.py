869a9d2fc302a985a8339b9a4deb7a6f631da50d
Julien Bloch <julien.bloch@berkeley.edu>
2017-09-13 15:22:14 -0700

    added heatmap and edited csv

---
+++ HalEvsB.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+EvsB = np.loadtxt(open("EvsB.csv", "rb"), delimiter=",", skiprows=0)
+
+P = EvsB[:,0]
+B = EvsB[:,2]
+V = 25*EvsB[:,4]
+
+bins = np.array([np.linspace(14, 29, num=5), np.linspace(-71, 71, num=30)])
+
+h = plt.hist2d(P, B, weights=V, bins=bins)
+plt.ylabel("Pressure in Torr")
+plt.xlabel("Magnetic Field in milliTesla")
+plt.title("Hall Voltage as Function of Pressure and Magnetic Field")
+plt.colorbar(h[3])
+plt.show()
